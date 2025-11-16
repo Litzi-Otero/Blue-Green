c29aed7e0ff23d5801256b061d8e9229e9de9d41
Litzi-Otero <2022371132@uteq.edu.mx>
2025-11-16 16:03:26 -0600

    edit header

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bienvenido a mi App Frontend Simple en React</h1>
+        <h1>Bienvenido a mi App - Litzi Otero</h1>
         <p>Versión: <span className="version">2.0.0 - Blue-Green Ready</span></p>
         <button className="counter-btn" onClick={handleClick}>
           Haz clic para incrementar
